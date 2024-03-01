e87b5527787ab520fdacffbaed6b0b52f5f9e403
Anthony Lukach <anthonylukach@gmail.com>
2024-02-29 19:30:48 -0800

    Rm need for client_secret

--- api/main.py
+++ api/main.py
@@ -15,7 +15,7 @@ class Settings(BaseSettings):
     keycloak_internal_url: Optional[str] = None
     keycloak_realm: str
     keycloak_client_id: str
-    keycloak_client_secret: str = Field(..., repr=False)
+    keycloak_client_secret: Optional[str] = Field(None, repr=False)
 
     @property
     def oidc_api(self):
@@ -31,12 +31,11 @@ class Settings(BaseSettings):
 settings = Settings(
     keycloak_realm="stac-api-playground-1",
     keycloak_client_id="stac-api",
-    keycloak_client_secret="0GFIvfdMnrz6vDCSHqoulZAOjlIflQPA",
 )
 
 oauth2_scheme = security.OAuth2AuthorizationCodeBearer(
     authorizationUrl=f"{settings.oidc_api}/auth",
-    tokenUrl=f"/token",
+    tokenUrl=f"{settings.oidc_api}/token",
     scopes={
         "stac:create": "Create STAC Items",
         "stac:read": "Read STAC Items",
@@ -88,34 +87,6 @@ def basic(user_token: Annotated[Dict[Any, Any], Depends(user_token)]):
     return user_token
 
 
-@app.post("/token", include_in_schema=False)
-async def token(
-    code: Annotated[str, Form(...)],
-    grant_type: Annotated[str, Form(...)],
-    client_id: Annotated[str, Form(...)],
-    redirect_uri: Annotated[str, Form(...)],
-    async_client: Annotated[httpx.AsyncClient, Depends(async_client)],
-    code_verifier: Annotated[Optional[str], Form(...)] = None,
-):
-    """
-    Authorization code to access token exchange endpoint. Allows us to get tokens
-    securely with our `client_secret`, enabling us to use the "client authentication"
-    and "client authorization" features within Keycloak.
-    """
-    return (
-        (
-            await async_client.post(
-                f"{settings.internal_oidc_api}/token",
-                data={
-                    "client_secret": settings.keycloak_client_secret,
-                    "client_id": client_id,
-                    "grant_type": grant_type,
-                    "code": code,
-                    "code_verifier": code_verifier,
-                    "redirect_uri": redirect_uri,
-                },
-            )
-        )
-        .raise_for_status()
-        .json()
-    )
+@app.get("/scope")
+def basic(user_token: Annotated[Dict[Any, Any], Depends(user_token)]):
+    return user_token["scope"].split(" ")
